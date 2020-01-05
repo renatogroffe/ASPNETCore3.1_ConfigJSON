921763d85d80762c9bc6ba80078ff6cb4447eb89
Renato Groffe <renato.groffe@yahoo.com.br>
2020-01-05 16:46:18 -0300

    ASP.NET Core 3.1 + Leitura configurações appsettings.json
    ASP.NET Core 3.1 + Leitura configurações appsettings.json

---
+++ APIDadosBrasil/Controllers/DadosController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using APIDadosBrasil.Models;
+
+namespace APIDadosBrasil.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DadosController : ControllerBase
+    {
+        [HttpGet]
+        public DadosPais Get([FromServices]DadosPais dados)
+        {
+            return dados;
+        }
+    }
+}
---
+++ APIDadosBrasil/Startup.cs
@@ -0,0 +1,52 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
+using APIDadosBrasil.Models;
+
+namespace APIDadosBrasil
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+           var dadosPais = new DadosPais();
+
+            new ConfigureFromConfigurationOptions<DadosPais>(
+                Configuration.GetSection("DadosPais"))
+                    .Configure(dadosPais);
+
+            services.AddSingleton(dadosPais);
+            
+            services.AddControllers();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
